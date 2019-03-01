182fb05a986ac1283b3b9471d329d7e88036f187
Jarno Virta <jarnovirta@users.noreply.github.com>
2019-03-01 22:43:38 +0200

    basic configuration done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("testi")
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
